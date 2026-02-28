fd46573e3a8fe671283bec18b9136c44e98fa1f2
Swami Malode <swamimalode2004@gmail.com>
2026-03-01 03:00:09 +0530

    github graph changes

--- src/components/Graph.tsx
+++ src/components/Graph.tsx
@@ -1,11 +1,23 @@
 import React from 'react'
 import GitHubCalendar from 'react-github-calendar';
 
-
 const Graph = () => {
   return (
-    <div className='bg-black flex justify-center p-4'>
-      <GitHubCalendar username="swamimalode07" blockRadius={3}/>
+    <div className='bg-black flex justify-center p-4 w-full'>
+      <div className='w-full flex justify-center'>
+        <GitHubCalendar 
+          username="swamimalode07" 
+          blockRadius={4}
+          blockSize={15}
+          blockMargin={6}
+          fontSize={16}
+          style={{ width: '100%' }}
+          theme={{
+            dark: ['#2a2a2a', '#909090', '#b8b8b8', '#dedede', '#ffffff'],
+          }}
+          colorScheme='dark'
+        />
+      </div>
     </div>
   )
 }
