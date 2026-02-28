06a3159e55ec3864297f6c844f1d050428ba35de
Swami Malode <swamimalode2004@gmail.com>
2026-02-28 18:03:53 +0530

    added shadcn and static colors in globals

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -43,20 +43,20 @@ const Navbar: React.FC = () => {
     <>
       <div className="border-b-2 border-[#1C1C1F] bg-black px-[2%]">
         <div className="flex items-center justify-between px-4 py-4 sm:px-6">
-          <p className="font-space-grotesk-700 font-sans text-lg font-semibold text-white sm:text-xl">
+          <p className="font-sans text-lg font-semibold text-white sm:text-xl">
             @code by SWAMI
           </p> 
 
           <div className="flex items-center gap-3 sm:gap-6 lg:gap-8">
             <div className="hidden items-center gap-6 md:flex lg:gap-8">
               <button
-                className="font-space-grotesk-300 font-sans font-medium text-white transition-colors hover:text-gray-300"
+                className=" font-sans font-medium text-white transition-colors hover:text-gray-300"
                 onClick={() => router.push("/projects")}
               >
                 Projects
               </button>
               <button
-                className="font-space-grotesk-300 font-sans font-medium text-white transition-colors hover:text-gray-300"
+                className=" font-sans font-medium text-white transition-colors hover:text-gray-300"
                 onClick={() => router.push("/artgallery")}
               >
                 Art Gallery
@@ -67,7 +67,7 @@ const Navbar: React.FC = () => {
               onClick={toggleCommandMenu}
               className="hidden items-center gap-3 rounded-lg border-2 border-[#252528] bg-black px-4 py-2.5 text-gray-400 transition-all duration-200 hover:border-gray-500 hover:text-gray-300 md:flex"
             >
-              <span className="font-space-grotesk-400 text-sm font-medium">
+              <span className=" text-sm font-medium">
                 Search...
               </span>
               <div className="flex items-center gap-1">
