ee2cc9cb4daf407ac769d2365ee72a0a638930d1
Swami Malode <swamimalode2004@gmail.com>
2026-03-03 17:45:34 +0530

    hero section completed

--- src/components/RingButton.tsx
+++ src/components/RingButton.tsx
@@ -16,7 +16,7 @@ const RingButton = ({ text, href, icon: Icon }: RingButtonProps) => {
         <Icon className="h-4 w-4 sm:h-5 sm:w-5" />
       )}
 
-      <span className="text-xs md:text-lg">
+      <span className="text-xs md:text-lg hover:text-white/70">
         {text}
       </span>
 
@@ -32,7 +32,7 @@ const RingButton = ({ text, href, icon: Icon }: RingButtonProps) => {
         className="relative flex items-center gap-2 bg-grey border border-[#39393D] px-6 py-2 md:py-4 rounded-md transition-colors"
       >
         <Link href={href}>
-          {content}
+          {content}s
         </Link>
       </Button>
     )
@@ -41,7 +41,7 @@ const RingButton = ({ text, href, icon: Icon }: RingButtonProps) => {
   return (
     <Button
       size="lg"
-      className="relative flex items-center gap-2 bg-[#27272A] border border-[#39393D] px-6 py-4 rounded-md transition-colors"
+      className="relative flex items-center gap-2 border border-[#39393D] px-6 py-4 rounded-md transition-colors"
     >
       {content}
     </Button>
