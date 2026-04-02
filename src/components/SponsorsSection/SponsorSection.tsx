f31cf78813c46d0498f723664288c6a6a16a8edb
Swami Malode <swamimalode2004@gmail.com>
2026-04-02 17:19:17 +0530

    added take this slot button

--- src/components/SponsorsSection/SponsorSection.tsx
+++ src/components/SponsorsSection/SponsorSection.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import SponsorCard from './SponsorCard'
+import TakethisSlot from './TakethisSlot'
 
 const SponsorSection = () => {
   return (
-     <div className="px-[4%] py-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+     <div className="px-[4%] py-10 grid grid-cols-3 gap-4 items-center justify-center">
         <SponsorCard/>
+        <TakethisSlot/>
     </div>
   )
 }
